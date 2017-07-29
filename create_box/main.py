8859a10104d462f2c6c4edec6dc21223bbb91848
Liz Parody <lizparody@gmail.com>
2017-07-29 11:41:30 -0400

    Added the nested for loop in the box exercise

--- create_box/main.py
+++ create_box/main.py
@@ -5,6 +5,18 @@ def create_box(height, width, character):
         box += '\n'
     return box
 
+#With a for nested loop
+"""
+
+def create_box(height, width, character):
+    box = ""
+    for i in range(height):
+      for j in range(width):
+        box += character
+      box += '\n'
+    return box
+"""
+
 # Tests:
 
 first_box_expected = """
