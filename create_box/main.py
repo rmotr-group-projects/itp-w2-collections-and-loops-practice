e17ae96fa9fca0c66dede2e57e6b65e80b6fc6e5
Fabricio <fabriciocaladomoreira@gmail.com>
2017-09-01 12:31:59 +0000

    Create_box first attempt

--- create_box/main.py
+++ create_box/main.py
@@ -1,5 +1,17 @@
 def create_box(height, width, character):
-    pass
+    
+    # Initializing box with empty string 
+    box = ''
+    
+    # Create a for loop that will loop the number of times in height
+    # the _ means that no variable name is needed in the for loop
+    for _ in range(height):
+        
+        # Multiplying the width by character and the endline
+        box += character*width +'\n'
+    
+    # Returning newly created box    
+    return box
 
 
 # Tests:
@@ -28,4 +40,7 @@ def test_first_box():
 def test_second_box():
     assert create_box(1, 1, '@') == second_box_expected
 
+def test_third_box():
+    assert create_box(3, 24, 'x') == third_box_expected
+    
 # Write your own test using the `third_box_expected` box
