265c1ed232484fa3bef4469061fe6ccadad35903
Phuc Tran <chubby018@gmail.com>
2017-09-02 23:58:53 +0000

    create box/border completed

--- create_box/main.py
+++ create_box/main.py
@@ -1,6 +1,19 @@
 def create_box(height, width, character):
-    pass
+    box = ''
+    for y in range(height):
+        box += character * width + '\n'
+    return box
 
+def create_border(height, width, character):
+    box = ''
+    for y in range(height):
+        if y == 0:
+            box += character * width + '\n'
+        elif y == height - 1:
+            box += character * width + '\n'
+        else:
+            box += character + ' ' * (width - 2)+ character + '\n'
+    return box
 
 # Tests:
 
@@ -20,6 +33,28 @@ xxxxxxxxxxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxxxxxxxxxxx
 """.lstrip()
 
+fourth_test = """
+#####
+#   #
+#   #
+#   #
+#   #
+#   #
+#   #
+#   #
+#####
+""".lstrip()
+
+fifth_test = """
+ooooooooooo
+o         o
+o         o
+o         o
+o         o
+o         o
+ooooooooooo
+""".lstrip()
+
 
 def test_first_box():
     assert create_box(3, 4, '*') == first_box_expected
@@ -28,4 +63,11 @@ def test_first_box():
 def test_second_box():
     assert create_box(1, 1, '@') == second_box_expected
 
-# Write your own test using the `third_box_expected` box
+def third_test_box():
+    assert create_box(3, 24, 'x') == third_box_expected
+    
+def test_first_border():
+    assert create_border(9, 5, '#') == fourth_test
+
+def test_second_border():
+    assert create_border(7, 11, 'o') == fifth_test
