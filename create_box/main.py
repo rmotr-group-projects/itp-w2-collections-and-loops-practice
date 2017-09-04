46ec3146bc827b5d93cf43a9934826025a892469
Dov Harrington <harringtondov@gmail.com>
2017-09-04 13:27:58 +0000

    Completed create_box including passing tests

--- create_box/main.py
+++ create_box/main.py
@@ -1,5 +1,14 @@
 def create_box(height, width, character):
-    pass
+    # capturing what each row of the box represents
+    row = character * width + '\n'
+    h = 1
+    box = row
+    while h < height:
+        h = h + 1
+        box += row
+    return box
+        
+print(create_box(1, 4, 'P'))
 
 
 # Tests:
@@ -24,8 +33,9 @@ xxxxxxxxxxxxxxxxxxxxxxxx
 def test_first_box():
     assert create_box(3, 4, '*') == first_box_expected
 
-
 def test_second_box():
     assert create_box(1, 1, '@') == second_box_expected
 
 # Write your own test using the `third_box_expected` box
+def test_third_box():
+    assert create_box(3, 24, 'x') == third_box_expected
