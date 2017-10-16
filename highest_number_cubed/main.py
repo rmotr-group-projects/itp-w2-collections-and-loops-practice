cdd9f5d54c292ee55d73d14acb63b6a25f738fab
yeli-buonya <yeli.e.buonya@gmail.com>
2017-10-15 19:24:29 -0700

    complete exercise

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,6 +1,19 @@
 def highest_number_cubed(limit):
-    pass
+    test_values = []
+    test_value = 1
+    cubed_number = test_value
 
+    def cube_number(value):
+        import math
+
+        return math.pow(value, 3)
+
+    while cubed_number <= limit:
+        test_values.append(test_value)
+        test_value += 1
+        cubed_number = cube_number(test_value)
+
+    return max(test_values)
 
 def test_three():
     assert highest_number_cubed(30) == 3
@@ -16,3 +29,9 @@ def test_one():
 
 def test_big():
     assert highest_number_cubed(12000) == 22
+
+
+highest_number_cubed(30)
+highest_number_cubed(12)
+highest_number_cubed(3)
+highest_number_cubed(12000)
