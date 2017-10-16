7a66f572700e1445906e45f235830597823da72c
Dan Tembe <dtembe@yahoo.com>
2017-10-15 22:40:35 -0500

    Updated Create Box -2

--- create_box/main.py
+++ create_box/main.py
@@ -1,16 +1,16 @@
 def create_box(height, width, character):
     row = width * character
-    for a in range(height):
+    for a in range(height - 1):
         print(row)
     return row
 
-
-height = 3
-width = 4
-character = '*'
+"""
+height = 1
+width = 1
+character = '@'
 testcode = create_box(height, width, character)
 print(testcode)
-
+"""
 
 
 # Tests:
@@ -40,3 +40,5 @@ def test_second_box():
     assert create_box(1, 1, '@') == second_box_expected
 
 # Write your own test using the `third_box_expected` box
+def test_third_box():
+    assert create_box(3, 24, 'x') == third_box_expected
