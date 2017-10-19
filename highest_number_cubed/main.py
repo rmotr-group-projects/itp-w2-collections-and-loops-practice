837d4a3390c1534fb2f321caf90e207e044d8e24
Adriana <aspatterson@gmail.com>
2017-10-19 16:34:42 +0000

    Adriana's solutions to create_box and highest_number_cubed

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,10 +1,20 @@
 def highest_number_cubed(limit):
-    pass
+    num = 1
+    answer = 0
+    while True:
+        answer = num ** 3
+        if answer > limit: 
+            num = num - 1
+            break
+        else:
+            num = num + 1
+    return num
+
+print(highest_number_cubed(30))
 
 
 def test_three():
     assert highest_number_cubed(30) == 3
-
 
 def test_two():
     assert highest_number_cubed(12) == 2
