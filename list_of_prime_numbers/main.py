5b246ad153fbdbb3fe70f5e44ac1702d4eca3fa6
Julia <jukostyuchenko@gmail.com>
2017-10-19 21:02:52 +0000

    list_of_prime_numbers working

--- list_of_prime_numbers/main.py
+++ list_of_prime_numbers/main.py
@@ -1,10 +1,19 @@
 
 def _is_prime(number):
-    pass
+    if number > 1:
+       for i in range(2, number):
+           if(number % i) == 0:
+               return False
+       else:
+           return True
 
 
 def list_of_prime_numbers(max_number):
-    pass
+    result = []
+    for number in range(1, (max_number + 1)):
+        if _is_prime(number):
+            result.append(number)
+    return result
 
 # =================== #
 # ====== Tests ====== #
