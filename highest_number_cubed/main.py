6d3564df44c05cd2503753b10f0195c098db5235
yeli-buonya <yeli.e.buonya@gmail.com>
2017-10-23 20:02:34 -0700

    add comments

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,20 +1,19 @@
 def highest_number_cubed(limit):
-    test_values = []
-    test_value = 1
-    cubed_number = test_value
+    test_values = [] # will hold all test values
+    test_value = 0 # initalize our starting value
+    cubed_number = test_value 
 
-
-    def cube_number(value):
-        import math
-
+    def cube_this(value):
+        import math               # function that returns a cubed value
         return math.pow(value, 3)
 
     while cubed_number <= limit:
         test_values.append(test_value)
         test_value += 1
-        cubed_number = cube_number(test_value)
+        cubed_number = cube_this(test_value)
 
-    return max(test_values)
+    return max(test_values) # will return the highest value in the array
+
 
 def test_three():
     assert highest_number_cubed(30) == 3
